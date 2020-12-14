759e6e1e3c892465b2c342c733c6b724d1ffcbd3
alec <alecbgarcia@gmail.com>
2020-12-14 12:29:50 -0700

    fixed footer final

--- client/src/components/pages/Footer.js
+++ client/src/components/pages/Footer.js
@@ -9,7 +9,7 @@ export default function Footer() {
 
   return (
     <div className={styles.footer_Container}>
-    <a href="https://github.com/alecbrando/Digitize" className={styles.smallContainer}><GitHubIcon  fontSize="large" className={styles.logos}/></a>
+    <a href="https://github.com/alecbrando/Twitchr" className={styles.smallContainer}><GitHubIcon  fontSize="large" className={styles.logos}/></a>
     <a href="https://www.linkedin.com/in/alec-garcia-4159b0169/" className={styles.smallContainer}><LinkedInIcon  fontSize="large" className={styles.logos}/></a>
     <a href="https://angel.co/u/alec-garcia-5" className={styles.smallContainer}><img height="60px" src="https://www.shareicon.net/data/128x128/2016/06/20/606409_angellist_4096x4096.png" className={styles.logos}/></a>
     </div>
